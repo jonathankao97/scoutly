9faacd6c8466a0f5d8233e80872871029894ffe6
Jonathan Kao <jkao97@yahoo.com>
2021-09-11 18:09:10 -0400

    feat: implement pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,6 +140,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "backend.pagination.CustomPagination",
+    "PAGE_SIZE": 10,
 }
 
 AUTH_USER_MODEL = "core.User"
