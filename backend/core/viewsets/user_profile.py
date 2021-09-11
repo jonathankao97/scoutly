f7e10b7ae87d5fa903b84df223cab3fa09607337
Jonathan Kao <jkao97@yahoo.com>
2021-09-11 19:40:18 -0400

    feat: add get_profiles endpoint

--- backend/core/viewsets/user_profile.py
+++ backend/core/viewsets/user_profile.py
@@ -1,8 +1,27 @@
 from rest_framework import viewsets
 from core.models import UserProfile
 from core.serializers import UserProfileSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from core.models import Interaction
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+    @action(detail=True, methods=["get"])
+    def get_profiles(self, request, pk):
+        queryset = self.filter_queryset(self.get_queryset())
+        seen_profiles = Interaction.objects.filter(sender__user_profile=pk).values(
+            "receiver"
+        )
+        queryset = queryset.exclude(pk=pk).exclude(user__in=seen_profiles)
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
